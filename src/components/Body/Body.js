2b9aa18a07b8e7abf445f5b8c4d0cf31d4b4adf1
sdshuvo98 <hellosdshuvo@gmail.com>
2022-04-09 16:45:52 +0600

    first commit with router, firebse, react-firebse-hooks

---
+++ src/components/Body/Body.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Body.css'
+
+const Body = () => {
+    return (
+        <body>
+            <p className='neon'>Kinbo<small>.com</small></p>
+        </body >
+    );
+};
+
+export default Body;
